d6cdeaf107d39f462abbbfefcf32e5193932d493
unknown <diegoruiz2936@gmail.com>
2023-09-01 08:45:31 -0500

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PruebaTecnica.NET.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
